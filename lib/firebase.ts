b81f74ff1bb4dca925287f9603d4103179c3b26e
moathssnck <myneww00@gmail.com>
2025-07-22 04:45:01 +0300

    sss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,7 +4,13 @@ import { getDatabase } from 'firebase/database';
 import { doc, getFirestore, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyDp_uI4zO6I3D5h8G1MK1bCAjIqH8prbJc",
+  authDomain: "tamins-86ba3.firebaseapp.com",
+  projectId: "tamins-86ba3",
+  storageBucket: "tamins-86ba3.firebasestorage.app",
+  messagingSenderId: "952748602321",
+  appId: "1:952748602321:web:04ef672e0d44b70d5566f0",
+  measurementId: "G-EB2WJPP7FP"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
