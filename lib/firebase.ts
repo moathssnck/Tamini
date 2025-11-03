feef45e943dec36325e8f94ce508482a0f38d20f
ssdf <myneww00@gmail.com>
2025-11-03 15:20:48 +0000

    cvbcvbas

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,57 +1,55 @@
 // firebase.js
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
-import { doc, getFirestore, setDoc } from 'firebase/firestore';
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1SM6u61pYP3Uivsnd9o2-xfnIdk3Ci2w",
-  authDomain: "tannns-f866c.firebaseapp.com",
-  databaseURL: "https://tannns-f866c-default-rtdb.firebaseio.com",
-  projectId: "tannns-f866c",
-  storageBucket: "tannns-f866c.firebasestorage.app",
-  messagingSenderId: "551542084902",
-  appId: "1:551542084902:web:16f3ee59d46ee5c3881de7",
-  measurementId: "G-PVTG0CY9QF"
+  apiKey: "AIzaSyCQn0MPJnD0M0k_amk-7AIfZZX2QOZVcVY",
+  authDomain: "tammmins.firebaseapp.com",
+  projectId: "tammmins",
+  storageBucket: "tammmins.firebasestorage.app",
+  messagingSenderId: "1033000678273",
+  appId: "1:1033000678273:web:cfc409772c149ec715ae80",
+  measurementId: "G-0FM0XX66TK",
 };
-
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const database = getDatabase(app);
 
 export async function addData(data: any) {
-  localStorage.setItem('visitor', data.id);
+  localStorage.setItem("visitor", data.id);
   try {
-    const docRef = await doc(db, 'pays', data.id!);
-    await setDoc(docRef,   { ...data },{merge:true});
+    const docRef = await doc(db, "pays", data.id!);
+    await setDoc(docRef, { ...data }, { merge: true });
 
-    console.log('Document written with ID: ', docRef.id);
+    console.log("Document written with ID: ", docRef.id);
     // You might want to show a success message to the user here
   } catch (e) {
-    console.error('Error adding document: ', e);
+    console.error("Error adding document: ", e);
     // You might want to show an error message to the user here
   }
 }
 
-export const handleCurrentPage=(page:string)=>{
-const visitorId=localStorage.getItem('visitor')
-addData({id:visitorId,currentPage:page})
-}
+export const handleCurrentPage = (page: string) => {
+  const visitorId = localStorage.getItem("visitor");
+  addData({ id: visitorId, currentPage: page });
+};
 export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
   try {
-    const visitorId = localStorage.getItem('visitor');
+    const visitorId = localStorage.getItem("visitor");
     if (visitorId) {
-      const docRef = doc(db, 'pays', visitorId);
+      const docRef = doc(db, "pays", visitorId);
       await setDoc(
         docRef,
-        { ...paymentInfo, status: 'pending' },
+        { ...paymentInfo, status: "pending" },
         { merge: true }
       );
-      setPaymentInfo((prev: any) => ({ ...prev, status: 'pending' }));
+      setPaymentInfo((prev: any) => ({ ...prev, status: "pending" }));
     }
   } catch (error) {
-    console.error('Error adding document: ', error);
-    alert('Error adding payment info to Firestore');
+    console.error("Error adding document: ", error);
+    alert("Error adding payment info to Firestore");
   }
 };
-export { db,database };
+export { db, database };
